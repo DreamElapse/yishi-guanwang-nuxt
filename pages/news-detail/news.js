1ca0c449a1cf1213fab2972dceb2bda502766309
DreamElapse <291839709@qq.com>
2020-12-16 09:54:31 +0800

    first commit

---
+++ pages/news-detail/news.js
@@ -0,0 +1,419 @@
+export default {
+  // 新闻一
+  news1:[
+    {
+      class: 'title',
+      text: '高铁上美食上新啦！温暖就是回家路上的一口“神仙米饭”呀！'
+    }, {
+      class: 'text',
+      text: '年初的银杏还在枝头灿烂，转眼我们又将和旅客朋友们一起告别2019，迎来庚子鼠年，迎接全新时代曙光在冬日缓缓升起，即将驱走寒冬， 带来人间温暖~'
+    },{
+      class: 'img',
+      imgWidth: '510px',
+      text: 'https://image1.cdn.yishizongheng.com/shejian_gw/news/news1/pic4.png'
+    },{
+      class: 'mark',
+      text: '“曙光”在路上'
+    },{
+      class: 'text',
+      text: '面对即将到来的春运大客流。为确保每一位旅客在奔波的路途中，享用上精美而有温度的美味餐食，让远方与家之间的距离变得不再那么 遥远我们早早做好了准备~'
+    },{
+      class: 'mark',
+      text: '“曙光”行动'
+    },{
+      class: 'text',
+      text: '2019的秋冬，迎着新生的希望，『 舌尖上的旅途 』产品研发小组再次踏上产品调研及研发的路途。以“曙光”为主题，展望2020，希望带 给旅客更多温暖的『 舌尖上的旅途 』研发团队，在半年多的时间里，穿梭城市之间，结合南北方旅客饮食习惯，力求从餐食营养配比， 食材搭配，原料追踪，个性化餐盒调配出最能满足广大旅客朋友味蕾需求的精品餐食，呈现给您一趟趟幸福、温暖、充满希望的舌尖之旅。'
+    },{
+      class: 'img',
+      imgWidth: '510px',
+      text: 'https://image1.cdn.yishizongheng.com/shejian_gw/news/news1/pic5.png'
+    },{
+      class: 'text',
+      text: '“神仙米饭”了解一下？我们对您的温暖承诺，从每一粒饱满的米饭就已开始~米饭于人们而言，早已不再只是一口饱腹之食，它代表着安 宁富足，触手可及的幸福生活粤语里说祝您“有米”，即有财，有曙光带给人无限希望和温暖。为了这份温暖，我们一直在努力~在本次更新的餐食中，最让人欲罢不能的就要数这一粒粒晶莹剔透的“米饭君”了！米和水的相遇，不浪费丝毫情意，完美糊化，紧密结合。被充分 滋养的饱满米粒，在万级洁净度的生产线上最终与其它食材相遇，成就了这一份份色香俱全的铁路餐食。'
+    },{
+      class: 'mark',
+      text: '万级洁净餐食生产线'
+    },{
+      class: 'text',
+      text: '粒粒透芯的米饭，搭配鲜虾的Q弹，排骨的鲜香，鸡肉的嫩滑，每一口下肚，都是一种多层次的味觉享受！很快让我们的旅客们卸下旅途重担，大快朵颐。'
+    },{
+      class: 'img',
+      imgWidth: '510px',
+      text: 'https://image1.cdn.yishizongheng.com/shejian_gw/news/news1/pic6.png'
+    },{
+      class: 'img',
+      imgWidth: '510px',
+      text: 'https://image1.cdn.yishizongheng.com/shejian_gw/news/news1/pic7.png'
+    },{
+      class: 'mark',
+      text: '我们未曾懈怠'
+    },{
+      class: 'text',
+      text: '我们对您的温暖承诺，更是从每一道质检，每一位专业保障人员开始 ~铁路餐饮对于餐食的要求可以用苛刻来形容。历经微生物、重金属、异物等多道检验，确认后的餐食要争分夺秒送上高铁餐车。需要中途加餐时，列车往往只停靠几分钟在这几分钟的间隙，和旅客们奔波的 步伐一起的，还有地面保障员们飞速而有条不紊的身影~'
+    },{
+      class: 'imgs',
+      imgWidth: '390px',
+      text: [
+        'https://image1.cdn.yishizongheng.com/shejian_gw/news/news1/pic8.png',
+        'https://image1.cdn.yishizongheng.com/shejian_gw/news/news1/pic9.png'
+      ]
+    },{
+      class: 'text',
+      text: '正是这样每一个朝升暮落的日子里，『 舌尖上的旅途 』各部门之间通力合作，持续发力，最终呈现给您这一份份有着鲜香口感的旅途佳肴。'
+    },{
+      class: 'imgs',
+      imgWidth: '390px',
+      text: [
+        'https://image1.cdn.yishizongheng.com/shejian_gw/news/news1/pic10.png',
+        'https://image1.cdn.yishizongheng.com/shejian_gw/news/news1/pic11.png'
+      ]
+    },{
+      class: 'texts',
+      text: ['2020年新一期餐食目前已全面上线，舌尖君已经迫不及待测评了上面提到的米饭君真的是 软！糯！香！甜！亲！测！好！吃！等您来品 鉴哦~', ' 曙光乍现，一切都将重新开始您的新年旅行list想必又添目的地，新的一年，『 舌尖上的旅途 』仍然翘首以盼，等候您来一起坐高铁， 赏美景，尝美食遍历山河~']
+    }
+  ],
+
+  // 新闻二
+  news2: [
+    {
+      class: 'title',
+      text: '兰州—上海虹桥新增开行动车组！首发日活动太精彩.....'
+    },{
+      class: 'img',
+      imgWidth: '510px',
+      text: 'https://image1.cdn.yishizongheng.com/shejian_gw/news/news2/pic1.png'
+    },{
+      class: 'texts',
+      text: ['旅客朋友们请注意~您的2019年度余额即将清零，在这辞旧迎新的日子里，让人鸡冻的消息来惹！', '受益于全国铁路年底大调图，兰州铁路局新增多条至东南沿海高铁线路，其中兰州西-上海虹桥动车组列车G3182/1次 已于昨日首发上线！']
+    },{
+      class: 'img',
+      imgWidth: '510px',
+      text: 'https://image1.cdn.yishizongheng.com/shejian_gw/news/news2/pic2.png'
+    },{
+      class: 'texts',
+      text: ['这下，上午牛肉拉面，下午魔都打卡的“半日交通生活圈”更更更方便啦！', '穿越西安、洛阳、南京三大古都，由苍茫金城驶入氤氲江南，这是一条贯通西北重镇-东南沿海城市，壮阔如画卷的线路。', '一路与多个5A级景区不期而遇——“东方雕塑馆”—麦积山石窟、世界文化遗产——龙门石窟、横卧太湖的鼋头渚，五湖环抱的同里古镇...']
+    },{
+      class: 'imgs',
+      imgWidth: '568px',
+      text: [
+        'https://image1.cdn.yishizongheng.com/shejian_gw/news/news2/pic3.png',
+        'https://image1.cdn.yishizongheng.com/shejian_gw/news/news2/pic4.png',
+        'https://image1.cdn.yishizongheng.com/shejian_gw/news/news2/pic5.png'
+      ]
+    },{
+      class: 'img',
+      imgWidth: '568px',
+      text: 'https://image1.cdn.yishizongheng.com/shejian_gw/news/news2/pic5.png'
+    },{
+      class: 'texts',
+      text: ['一路美景，怎能没有美食相伴！说起到上海必去的地方，咖啡馆必须拥有姓名~饮一杯浓郁咖啡，梦回一段旧日时光', '着力打造餐车特色服务，秉持“享您所想”服务理念的「舌尖上的旅途 」，把每一次新的启程当作使命，结合线路特点，不断升级餐饮产品 和服务模式以打造更多符合旅客期待的精品服务线路；', '新车首发当天，兰州国旅联合「舌尖上的旅途 」，结合兰州、上海两地的地域风俗和饮食习惯，共同打造「美酒加咖啡，醇醉兰沪行」主 题服务，把这份平淡生活里的精致带上了高铁。', '一场浪漫的醇醉之旅暖心开启~咖啡，美酒，仿佛置身旧电影场景。话不多说，现场视频先睹为快~人美景美，首发日更多精彩实况，快来 跟随小编镜头一起康康吧~']
+    },{
+      class: 'imgs',
+      imgWidth: '390px',
+      text: [
+        'https://image1.cdn.yishizongheng.com/shejian_gw/news/news2/pic6.png',
+        'https://image1.cdn.yishizongheng.com/shejian_gw/news/news2/pic7.png',
+      ]
+    },{
+      class: 'imgs-text',
+      imgWidth: '568px',
+      image: [
+        'https://image1.cdn.yishizongheng.com/shejian_gw/news/news2/pic8.png',
+        'https://image1.cdn.yishizongheng.com/shejian_gw/news/news2/pic9.png',
+      ],
+      text: ['列车发出前，乘务员们整装待发，元气满满~', '站台工作人员手持“上海方言”趣味板欢迎首批乘车旅客']
+    },{
+      class: 'text',
+      text: '走进餐车，鲜活的老上海风华让旅客们惊喜不已，古典的留声机，老式电话机，馥郁的白玉兰，旧式的招贴海报，仿佛跌入一个截然不同的时空中...'
+    },{
+      class: 'imgs',
+      imgWidth: '390px',
+      text: [
+        'https://image1.cdn.yishizongheng.com/shejian_gw/news/news2/pic10.png',
+        'https://image1.cdn.yishizongheng.com/shejian_gw/news/news2/pic11.png',
+      ]
+    },{
+      class: 'imgs',
+      imgWidth: '390px',
+      text: [
+        'https://image1.cdn.yishizongheng.com/shejian_gw/news/news2/pic12.png',
+        'https://image1.cdn.yishizongheng.com/shejian_gw/news/news2/pic13.png',
+      ]
+    },{
+      class: 'text',
+      text: '一袭典雅旗袍，惊艳现身的乘务人，燕燕婉笑，慢斟红酒，清唱经典怀旧歌曲，诚邀旅客朋友们前来品鉴。'
+    },{
+      class: 'img',
+      imgWidth: '510px',
+      text: 'https://image1.cdn.yishizongheng.com/shejian_gw/news/news2/pic14.png'
+    },{
+      class: 'text',
+      text: '香醇甘甜的红酒滑入高脚杯，晶莹剔透的杯身映射着窗外的西北大地，大有“葡萄美酒夜光杯”的畅饮豪爽，和“爱意醇浓斟满杯”的婉约浪 漫。'
+    },{
+      class: 'imgs',
+      imgWidth: '390px',
+      text: [
+        'https://image1.cdn.yishizongheng.com/shejian_gw/news/news2/pic15.png',
+        'https://image1.cdn.yishizongheng.com/shejian_gw/news/news2/pic16.png',
+      ]
+    },{
+      class: 'text',
+      text: '身着国际灰制服的专业咖啡师，就着咖啡的浓郁香气，为旅客朋友们现场演绎了一场特色拉花艺术，手臂一扬一落间，一个个生动图案跃然杯面。'
+    },{
+      class: 'imgs-text',
+      imgWidth: '510px',
+      image: [
+        'https://image1.cdn.yishizongheng.com/shejian_gw/news/news2/pic17.png'
+      ],
+      text: ['“曙光乍现”的萌鼠']
+    },{
+      class: 'text',
+      text: '餐车内言笑款款，车厢内同样舒适惬意，我们为旅客精心预备了兰州本地三泡茶，距离目的地时间尚早，倚窗远眺，呷一口茶心和胃在寒冬里暖暖的~'
+    },{
+      class: 'imgs',
+      imgWidth: '568px',
+      text: [
+        'https://image1.cdn.yishizongheng.com/shejian_gw/news/news2/pic18.png',
+        'https://image1.cdn.yishizongheng.com/shejian_gw/news/news2/pic19.png',
+      ]
+    },{
+      class: 'text',
+      text: '不仅“暖胃”，本次醇醉之行，舌尖上的旅途还为购买咖啡饮品的旅客们精心准备了“护手”心意——上海雪花膏；'
+    },{
+      class: 'imgs-text',
+      imgWidth: '510px',
+      image: [
+        'https://image1.cdn.yishizongheng.com/shejian_gw/news/news2/pic20.png'
+      ],
+      text: ['看外观洁白如雪；涂在脸上冰凉的感觉就像雪花掉落；抹开后白色膏体很快消失，犹如雪花融化-故名雪花膏']
+    },{
+      class: 'text',
+      text: '与我们一同到达目的地的还有即将告别的2019年,这一年，我们在忙碌与充实中度过，攻坚暑运，备战春运,在全国铁路版图上开疆辟土,践行着“一路有我，享您所想”'
+    },{
+      class: 'text',
+      text: '2020，长风破浪，未来可期，舌尖上的旅途不止今日，在平凡旅途中的每一日，都将以优质的服务，沉浸式的用餐体验，与您共度每一段相遇时光。'
+    },{
+      class: 'text',
+      text: '再见2019，2020年继续——爱您爱您；还未出发的旅客朋友们，赶紧mark起新景点出发咯~我们旅途见~'
+    }
+  ],
+
+  // 新闻三
+  news3: [
+    {
+      class: 'title',
+      text: '【北京冬奥会限量】教你轻松拥有未来三年最IN萌物~'
+    },{
+      class: 'text',
+      text: '9月17日晚，在首钢冰球馆，本年度最强CP、本年度最萌冰雪组合、北京2022年冬奥会和冬残奥会吉祥物——“冰墩墩”“雪容融”正式亮 相！'
+    },{
+      class: 'imgs-text',
+      imgWidth: '348px',
+      imgHeight: '196px',
+      isShow:true,
+      image: [
+        'https://image1.cdn.yishizongheng.com/shejian_gw/news/news3/pic1.png',
+        'https://image1.cdn.yishizongheng.com/shejian_gw/news/news3/pic2.png',
+      ],
+      text: ['北京冬奥会吉祥物，中文名：冰墩墩，英文名：Bing Dwen Dwen', '北京冬残奥会吉祥物，中文名：雪容融，英文名：Shuey Rhon Rhon']
+    },{
+      class: 'text',
+      text: '尽管时间还徘徊在2019的冬天，但这两个小可爱的亮相依然把我们提前带到了那个即将因竞技体育而沸腾的冬季。'
+    },{
+      class: 'mark',
+      text: '冬奥会知多少?'
+    },{
+      class: 'texts',
+      text: ['冬季奥林匹克运动会简称为冬季奥运会、冬奥会。主要由全世界各地区举行，是世界规模最大的冬季综合性运动会，每四年举办一届， 1994年起与夏季奥林匹克运动会相间举行。参与国主要分布在世界各地，包括欧洲、非洲、美洲、亚洲、大洋洲。',
+        '自2017年7月31日国际奥委会主席宣布中国北京获得2022年冬奥会举办权开始，全民冰雪运动浪潮开启！这将带动3亿人参与到冰雪运动 中！也将使得北京成为第一个举办夏季奥运会和冬季奥运会的城市。'
+      ]
+    },{
+      class: 'mark',
+      text: '北京冬奥会小课堂'
+    },{
+      class: 'img',
+      imgWidth: '480px',
+      text: 'https://image1.cdn.yishizongheng.com/shejian_gw/news/news3/pic3.png',
+      isShow:true
+    },{
+      class: 'text',
+      text: '中文名称：第24届冬季奥林匹克运动会<br />英语名：XXIV Olympic Winter Games<br />时间：2022年2月4日至2022年2月20日<br />地点：中华人民共和国北京市和河北省张家口市<br />比赛场地：分为北京城区、延庆区、张家口三个赛区<br />身份：中国历史上第一届冬季奥运会，继北京2008年奥运会、南京2014年青奥会之后中国第三次举办的奥运赛事。'
+    },{
+      class: 'texts',
+      text: ['说起现代奥运会，深得民心的吉祥物往往和赛事一样重重重要~自1968年的格勒诺布尔冬奥会起，就开始有了设计吉祥物的传统。',
+        '下面一图带你了解下“冰墩墩”和”雪容融”的各位“前辈”'
+      ]
+    },{
+      class: 'imgs-text',
+      imgWidth: '510px',
+      image: ['https://image1.cdn.yishizongheng.com/shejian_gw/news/news3/pic4.png'],
+      text: ['图片来自网络']
+    },{
+      class: 'texts',
+      text: ['看完之后，大家是不是觉得我们本届两位小精灵愈发的娇俏可爱了呢！',
+        '别看我们“冰墩墩”呆萌呆萌的，人家可是一只有着超能量冰晶外壳的熊猫呢~'
+      ]
+    },{
+      class: 'img',
+      imgWidth: '620px',
+      text: 'https://image1.cdn.yishizongheng.com/shejian_gw/news/news3/pic5.png',
+      isShow:true
+    },{
+      class: 'text',
+      text: '“冰墩墩”造型亮点：头盔面镜周围装饰有七彩斑斓的彩色光环，灵感源自于北京冬奥会的国家速滑馆——“冰丝带”；它的整体形象酷似航 天员，寓意创造非凡、探索未来，体现了追求卓越、引领时代，以及面向未来的无限可能。“'
+    },{
+      class: 'text',
+      text: '另一只它的小伙伴—“雪容融”则是一个有着正统中华文化血统的灯笼娃娃。'
+    },{
+      class: 'img',
+      imgWidth: '620px',
+      text: 'https://image1.cdn.yishizongheng.com/shejian_gw/news/news3/pic6.png',
+      isShow:true
+    },{
+      class: 'texts',
+      text: ['“雪容融”设计亮点：以灯笼为原型，顶部的如意造型象征吉祥幸福；装饰图案融入了中国传统剪纸艺术；面部的雪块既代表“瑞雪兆丰年” 的寓意，又体现了拟人化的设计，凸显吉祥物的可爱。',
+        '两个“冰”“雪”小精灵一亮相立马吸引了全民关注的目光，大家纷纷表示要被这两个小可爱萌翻啦~~'
+      ]
+    },{
+      class: 'img',
+      imgWidth: '620px',
+      text: 'https://image1.cdn.yishizongheng.com/shejian_gw/news/news3/pic7.png',
+      isShow:true
+    },{
+      class: 'texts',
+      text: ['可是！！比起远程在线撸萌物，能把这么可爱的萌物实实在在抱回家才最安心鸭！',
+        '为支持北京冬奥会举办，「舌尖上的旅途」提前牵手“冰墩墩”和”雪容融”早已把各种萌物周边摆上高铁啦！',
+        '近日，只要您乘坐「舌尖上的旅途」担当车次，即可在旅途中快人一步，提前迎接北京2022年冬季奥运会的到来！'
+      ]
+    },{
+      class: 'imgs',
+      imgWidth: '568px',
+      text: [
+        'https://image1.cdn.yishizongheng.com/shejian_gw/news/news3/pic8.png'
+      ]
+    },{
+      class: 'text',
+      text: '憨态可掬，饱含文化感的“冰墩墩”和“雪容融”，无论收藏或是送人，都有意又有面哦！'
+    },{
+      class: 'imgs',
+      imgWidth: '510px',
+      text: [
+         'https://image1.cdn.yishizongheng.com/shejian_gw/news/news3/pic9.png',
+         'https://image1.cdn.yishizongheng.com/shejian_gw/news/news3/pic10.png']
+    },{
+      class: 'text',
+      text: '与此同时，还有多款北京冬奥会吉祥物特许周边产品也在高铁上线啦！'
+    },{
+      class: 'imgs-text',
+      imgWidth: '510px',
+      image: ['https://image1.cdn.yishizongheng.com/shejian_gw/news/news3/pic11.png'],
+      text: ['“妈妈快给我买冰墩墩和雪融融~”']
+    },{
+      class: 'text',
+      text: '这等重大赛事，怎么能少得了极富纪念意义的徽章，快往下滑，看看集齐一套有没有冬奥会神龙召唤~'
+    },{
+      class: 'mark',
+      text: '北京冬奥会会徽吉祥物双枚纪念章'
+    },{
+      class: 'img',
+      imgWidth: '510px',
+      text: 'https://image1.cdn.yishizongheng.com/shejian_gw/news/news3/pic12.png',
+      isShow:true
+    },{
+      class: 'texts',
+      text: ['该套纪念章共两枚，铜质镀金和铜质镀银各一枚，规格为直径40毫米，造币工艺制造。',
+        '双枚纪念章正面图案分别为北京2022年冬奥会会徽、吉祥物以及中英文名称。背面图案为北京天坛和含有“2022”云带的祥云。',
+        '该套纪念章设计新颖，工艺精良，配有北京2022年冬奥会特许商品防伪标签，发行数量30000套。'
+      ]
+    },{
+      class: 'mark',
+      text: '雪花拼图徽章套装'
+    },{
+      class: 'img',
+      imgWidth: '510px',
+      text: 'https://image1.cdn.yishizongheng.com/shejian_gw/news/news3/pic13.png',
+      isShow:true
+    },{
+      class: 'texts',
+      text: ['冬奥会运动异彩纷呈，五枚异形徽章拼合成晶莹剔透的雪花形状，雪山、雪花等冬季特有元素为冬奥会增添了独特的气息，现代化比赛场 馆承载着冬奥会的动感与激情，讲述冬奥会的精彩故事。',
+        '套装可拆分为5枚独立徽章，可便于佩戴或展示。该设计获得冬奥有我创意设计大赛 创意设计奖。',
+        '这款徽章是样式最独特、唯美的，是给女性准备礼物的首选佳品！'
+      ]
+    },{
+      class: 'mark',
+      text: '短道速滑分解动作徽章套装'
+    },{
+      class: 'img',
+      imgWidth: '510px',
+      text: 'https://image1.cdn.yishizongheng.com/shejian_gw/news/news3/pic14.png',
+      isShow:true
+    },{
+      class: 'texts',
+      text: ['在刚落下帷幕的平昌冬奥会上，短道速滑项目为中国队摘下第一块奖牌，让更多国人认识了这项冰雪运动。',
+        '徽章套装以短道速滑分解动作为灵感，双重人影的设计巧妙地展现出风驰电擎的运动速度，构成一道道流动的风景线，突破自我，突破极 限，将奋力拼搏的瞬间永远定格。',
+        '套装共4枚徽章。非常具有激励作用。'
+      ]
+    },{
+      class: 'mark',
+      text: '短道速滑分解动作徽章套装'
+    },{
+      class: 'img',
+      imgWidth: '510px',
+      text: 'https://image1.cdn.yishizongheng.com/shejian_gw/news/news3/pic15.png',
+      isShow:true
+    },{
+      class: 'texts',
+      text: ['冬奥会会徽和吉祥物徽章，共计4枚套装。',
+        '红黄两色源自中国国旗，代表运动的激情、喜庆、青春与活力。',
+        '这款包含吉祥物和冬奥会、冬残奥会的会徽共四枚，整体小巧精致，又比较齐全。'
+      ]
+    },{
+      class: 'mark',
+      text: '历届冬季奥运会会徽徽章套装'
+    },{
+      class: 'img',
+      imgWidth: '510px',
+      text: 'https://image1.cdn.yishizongheng.com/shejian_gw/news/news3/pic16.png',
+      isShow:true
+    },{
+      class: 'text',
+      text: '会徽是见证和纪念历届奥林匹克的经典符号，套装集合了23枚历届冬奥会会徽及1枚历史知识产权标志，将奥林匹克精神与价值传播到世界 各地。具有较高的收藏价值。'
+    },{
+      class: 'text',
+      text: '少年强则中国强，国之重事；升腾的希望会在少年心中埋下希望的种子；一套兼具纸的温度与冰雪热情的文具本册诞生了。'
+    },{
+      class: 'mark',
+      text: '学生流沙本册-冰墩墩'
+    },{
+      class: 'img',
+      imgWidth: '510px',
+      text: 'https://image1.cdn.yishizongheng.com/shejian_gw/news/news3/pic17.png',
+      isShow:true
+    },{
+      class: 'text',
+      text: '此款设计将雪山的造型以扁平化的设计方式呈现，本皮采用环保材质，内部雪花和冰墩墩填充，可以在本皮内自由的流动、增加实用性的同时又具有趣味性。'
+    },{
+      class: 'mark',
+      text: '冰雪冬奥本册-多色可选'
+    },{
+      class: 'imgs-text',
+      imgWidth: '1000px',
+      image: ['https://image1.cdn.yishizongheng.com/shejian_gw/news/news3/pic18.png'],
+      text: ['请认准带有防伪标签的正版官方特许商品，购买特许商品是对北京冬奥会的支持！']
+    },{
+      class: 'texts',
+      text: ['冬奥会是一场冬季极限赛事的狂欢派对，冬天是冰雪的季节，舞动的雪花，好像一个个欢乐的音符，舞动在洁白的山脉之上，为冬日增添 温馨的画面，寓意着即将迎来一场冰雪的盛宴。实用性强，意义深刻。',
+        '想象一下，2022年北京冬奥会，当你拿出3年前珍藏下的本册，重新翻开今日写下的对冬奥会的祝福字句，是否希望的种子已经发芽，时 光已经承载了满满的回忆与感动呢？'
+      ]
+    },{
+      class: 'texts',
+      text: ['随着北京冬奥会的临近，将会有越来越多的特许商品陆续上线，在「舌尖上的旅途」担当次列车上与您见面。欢迎您随时咨询餐车乘务人 员，快人一步，带冰雪大家族回家哦！',
+        '希望即在不远的未来，2022我们将共襄盛举，2019的冬天，就让这些提前到来的精灵，陪伴您一起”冰”“雪”相融，共同期待！'
+      ]
+    },
+
+
+
+  ]
+}
+
